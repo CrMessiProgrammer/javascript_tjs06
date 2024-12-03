fcf973ca718390a7480a77e807232892107c9b19
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2024-12-03 17:34:29 -0300

    Aula 02 - Atualizações

--- aula_02/Logicos.js
+++ aula_02/Logicos.js
@@ -11,27 +11,27 @@ console.log("\nTela de Login")
 
 let usuarioLogin = leia.question("Digite o seu usuario: ")
 
-// 'hideEchoBack: true' deixa oculto o que o usuário vai digitar
+// 'hideEchoBack: true' deixa oculto (com) o que o usuário vai digitar
 let senhaLogin = leia.question("Digite a sua senha: ", {hideEchoBack: true})
 
 if(usuarioLogin === usuário && senhaLogin === senha) {
     var mensagem = "Seja Bem-Vindo! Usuário autenticado com sucesso!"
+
+    // negação
+    if(!isBlock){
+        console.log("Usuário está ativo!")
+        
+        if(isAdmin || isGerente){
+            console.log("Acesso ao painél Administrativo Liberado!")
+        } else {
+            console.log("Acesso ao painél Administrativo Negado!")
+        }
+    } else {
+        console.log("Usuário está inativo! Contate o Administrador!")
+            process.exit(0) // reseta, sai do programa ao executá-lo
+        }
 } else {
     var mensagem = "Acesso Negado!"
 }
 
 console.log(mensagem)
-
-// negação ()
-if(!isBlock){
-    console.log("Usuário está ativo!")
-} else {
-    console.log("Usuário está inativo! Contate o Administrador!")
-    process.exit(0) // reseta, sai do programa ao executá-lo
-}
-
-if(isAdmin || isGerente){
-    console.log("Acesso ao painél Administrativo Liberado!")
-} else {
-    console.log("Acesso ao painél Administrativo Negado!")
-}
